8ed7f0a39f0646b5ea133dafce8cb87bdf815589
Bora kalkan <borakalkan350@gmail.com>
2025-05-04 18:53:49 +0300

    Commit

--- Film_Dizi_API/Film_Dizi_API/obj/Debug/net8.0/Film_Dizi_API.AssemblyInfo.cs
+++ Film_Dizi_API/Film_Dizi_API/obj/Debug/net8.0/Film_Dizi_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Film_Dizi_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a85756a6ceff456523813e65e2d79e9cee91f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a343d17688f6b264aff5d2f4db59169891e575f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Film_Dizi_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Film_Dizi_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
