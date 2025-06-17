e5d1bc5d388ce1ce2349c3ce93a1be89c1580a4d
BoraKalkann <borakalkan350@hotmail.com>
2025-06-17 13:18:18 +0300

    register eklendi

--- Film_Dizi_API/Film_Dizi_API/Controllers/AccountController.cs
+++ Film_Dizi_API/Film_Dizi_API/Controllers/AccountController.cs
@@ -1,7 +1,8 @@
 ﻿using Film_Dizi_API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using Film_Dizi_API.DTOs.Account;
+using Film_Dizi_API.Data;   
 
 namespace Film_Dizi_API.Controllers
 {
@@ -14,6 +15,42 @@ namespace Film_Dizi_API.Controllers
         {
             _userManager = userManager;
         }
-        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var User = new User
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email
+                };
+
+                var createdUser = await _userManager.CreateAsync(User, registerDto.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(User, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User Created!");
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            }catch(Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
     }
 }
--- Film_Dizi_API/Film_Dizi_API/Data/ApplicationDbContext.cs
+++ Film_Dizi_API/Film_Dizi_API/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Film_Dizi_API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -15,8 +16,28 @@ namespace Film_Dizi_API.Data
         public DbSet<Film> Films { get; set; }
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                 new IdentityRole
+                {
+                     Id = "2",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
 
-      
-        
+
+
     }
 }
